e22bdaec28fbe7f10a80785af2a5d941d1dea0ff
udf <tabhooked@gmail.com>
2021-04-25 00:53:58 +0200

    switch to a group per storage folder
    enforced with ACL:
    chmod g+rwxs,o-rwx /booty
    setfacl -d -m g::rwx -m o::--- /booty

--- configuration.nix
+++ configuration.nix
@@ -48,19 +48,18 @@ in
   systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
 
   # Groups
-  users.groups.storage = {};
+  users.groups.st_music = {
+    members = [ "sam" "syncthing" ];
+  };
 
   # User accounts
   users.users = {
     sam = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "storage" ];
+      extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzlWx6yy2nWV8fYcIm9Laap8/KxAlLJd943TIrcldSY archdesktop"
       ];
-    };
-    syncthing = {
-      extraGroups = [ "storage" ];
     };
   };
 
