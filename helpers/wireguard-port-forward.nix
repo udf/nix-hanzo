b9d892ca7e6ef4b9891d68ba07eb4d146c0ab1cb
udf <tabhooked@gmail.com>
2021-06-27 02:19:35 +0200

    which idiot wrote this

--- helpers/wireguard-port-forward.nix
+++ helpers/wireguard-port-forward.nix
@@ -21,28 +21,17 @@ let
     ${iptables} ${getTableArg table} -X ${prefixChain chain}
   '';
   getForwardRules = {proto ? "tcp", port, ip}: ''
-    ${iptables} -A ${prefixChain "FORWARD"} -i ${externalInterface} -o ${interface} -p ${proto} ${lib.optionalString (proto == "tcp") "--syn"} --dport ${port} -m conntrack --ctstate NEW -j ACCEPT
     ${iptables} -t nat -A ${prefixChain "PREROUTING"} -i ${externalInterface} -p ${proto} --dport ${port} -j DNAT --to-destination ${ip}
-    ${iptables} -t nat -A ${prefixChain "POSTROUTING"} -o ${interface} -p ${proto} --dport ${port} -d ${ip} -j SNAT --to-source ${gatewayIP}
   '';
 in
 (removeAttrs cfg ["forwardedTCPPorts" "forwardedUDPPorts"]) // {
   postSetup = (cfg.postSetup or "") + ''
-    ${createChain {table="nat"; chain="POSTROUTING";}}
     ${createChain {table="nat"; chain="PREROUTING";}}
-    ${createChain {chain="FORWARD";}}
-
-    ${iptables} -t nat -A ${prefixChain "POSTROUTING"} -s ${gatewaySubnet} -o ${externalInterface} -j MASQUERADE
-
-    ${iptables} -A ${prefixChain "FORWARD"} -i ${externalInterface} -o ${interface} -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-    ${iptables} -A ${prefixChain "FORWARD"} -i ${interface} -o ${externalInterface} -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
     ${builtins.concatStringsSep "\n" (lib.mapAttrsToList (port: ip: (getForwardRules {proto="tcp"; port=port; ip=ip;})) forwardedTCPPorts)}
     ${builtins.concatStringsSep "\n" (lib.mapAttrsToList (port: ip: (getForwardRules {proto="udp"; port=port; ip=ip;})) forwardedUDPPorts)}
   '';
   postShutdown = (cfg.postShutdown or "") + ''
-    ${deleteChain {table="nat"; chain="POSTROUTING";}}
     ${deleteChain {table="nat"; chain="PREROUTING";}}
-    ${deleteChain {chain="FORWARD";}}
   '';
 }
