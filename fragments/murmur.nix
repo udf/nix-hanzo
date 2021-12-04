b919c4b03af7647f10b668a25775e3d7731aa1a3
udf <tabhooked@gmail.com>
2021-12-05 01:40:06 +0200

    I herby dub thee Sir Tificate

--- fragments/murmur.nix
+++ fragments/murmur.nix
@@ -9,7 +9,11 @@ in
     welcometext = "Congratulation! You have earned one (1) autism!";
     bandwidth = 144000;
     password = private.murmurPassword;
+    sslCert = "/var/lib/acme/tsunderestore.io/fullchain.pem";
+    sslKey = "/var/lib/acme/tsunderestore.io/key.pem";
   };
+
+  users.groups.nginx.members = [ "murmur" ];
 
   networking.firewall = {
     allowedUDPPorts = [ port ];
