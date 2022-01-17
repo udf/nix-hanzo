ed79a7d29a0cb93bff50ec6051188a6985b8d293
udf <tabhooked@gmail.com>
2022-01-17 03:16:03 +0200

    watcher: use log priority constants

--- constants/watcher-config.py
+++ constants/watcher-config.py
@@ -1,5 +1,6 @@
 import re
 import os
+from systemd.journal import LOG_INFO, LOG_NOTICE
 
 owner = 232787997
 token = os.environ['TOKEN']
@@ -9,18 +10,18 @@ flood_server_url = 'http://127.0.0.1:3000'
 def systemd_should_ignore(e):
   source = e.get('_COMM') or e.get('SYSLOG_IDENTIFIER')
   if source == 'kernel':
-    return e['PRIORITY'] >= 6
+    return e['PRIORITY'] >= LOG_INFO
   if source != 'systemd':
     # ignore non-systemd notice and below
-    return e['PRIORITY'] >= 5
+    return e['PRIORITY'] >= LOG_NOTICE
 
   unit = e.get('UNIT', '')
   return (
-    e['PRIORITY'] >= 5 and (
+    e['PRIORITY'] >= LOG_NOTICE and (
       unit == 'backup-root.service'
     )
   ) or (
-    e['PRIORITY'] >= 6 and (
+    e['PRIORITY'] >= LOG_INFO and (
       re.match(r'zfs-snapshot-\w+\.service', unit)
       or unit == 'systemd-tmpfiles-clean.service'
     )
