630cf1491de31a9e80ff051a27590e5343104a0d
udf <tabhooked@gmail.com>
2022-01-27 02:24:44 +0200

    useless goddess, useless vm

---
+++ configuration-aqua.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  imports = [
+    <nixpkgs/nixos/modules/virtualisation/google-compute-image.nix>
+
+    # core
+    ./fragments/system-packages.nix
+    ./fragments/users.nix
+
+    # services
+    ./modules/watcher-bot.nix
+
+    # programs
+    ./fragments/nvim.nix
+    ./fragments/zsh.nix
+  ];
+
+  services.openssh = {
+    enable = true;
+    ports = [ 69 ];
+    openFirewall = true;
+  };
+
+  swapDevices = [ { device = "/swapfile"; size = 2048; } ];
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 0;
+  };
+
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+  };
+}
