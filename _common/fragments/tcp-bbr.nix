9536ce77058f2cf536ec3817b1ada24129d04f47
udf <tabhooked@gmail.com>
2022-04-28 21:32:49 +0200

    shh bbr is ok

---
+++ _common/fragments/tcp-bbr.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  boot.kernel.sysctl = {
+    "net.core.wmem_max" = "12582912";
+    "net.core.rmem_max" = "12582912";
+    "net.ipv4.tcp_rmem" = "4096	524000	16777216";
+    "net.ipv4.tcp_wmem" = "4096	524000	16777216";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+  };
+}
