98cab8d48bd5c93d890cc2142cd2985d5b08d0f4
udf <tabhooked@gmail.com>
2022-04-28 17:34:04 +0200

    can congestion control fix my stuffy nose?

--- kurisu/configuration.nix
+++ kurisu/configuration.nix
@@ -38,6 +38,15 @@
     firewall.logRefusedConnections = false;
   };
 
+
+  boot.kernel.sysctl = {
+    "net.core.wmem_max" = "12582912";
+    "net.core.rmem_max" = "12582912";
+    "net.ipv4.tcp_rmem" = "4096	524000	16777216";
+    "net.ipv4.tcp_wmem" = "4096	524000	16777216";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+  };
+
   system.autoUpgrade = {
     enable = true;
     allowReboot = true;
