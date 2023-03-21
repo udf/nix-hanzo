4b7218ba459ea01ad25a24e3b692e53bcbe3af63
udf <tabhooked@gmail.com>
2023-03-21 18:46:53 +0200

    hey kids, backup your keys

--- _common/fragments/users.nix
+++ _common/fragments/users.nix
@@ -6,7 +6,7 @@
       extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzlWx6yy2nWV8fYcIm9Laap8/KxAlLJd943TIrcldSY archdesktop"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISRXQoctePFQfGqILvQHzoIgdeF1SPGxGRQBrfqWgoA phone"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/M7Ba3GQSuRFjMTInCAv/mZIvlc4KxyrJZkklL0yhv phone"
       ];
       uid = 1000;
     };
