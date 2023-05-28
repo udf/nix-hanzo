0034d721aac1fb7c35110429a6bf3636cb4b46a8
udf <tabhooked@gmail.com>
2023-05-28 20:06:47 +0200

    add laptop key

--- _common/fragments/users.nix
+++ _common/fragments/users.nix
@@ -7,6 +7,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzlWx6yy2nWV8fYcIm9Laap8/KxAlLJd943TIrcldSY archdesktop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/M7Ba3GQSuRFjMTInCAv/mZIvlc4KxyrJZkklL0yhv phone"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcNGC8Qg4+lVODt4cdbDtjbrVe44GGBae5sVoCZ1irJ sam@alice"
       ];
       uid = 1000;
       packages = [ pkgs.python310 pkgs.python310Packages.pip ];
