52697cd52dbaa9ba713746be1f905533ca2f0758
udf <tabhooked@gmail.com>
2023-10-14 01:13:54 +0200

    use fq qdisc
    i dont think this changes anything

--- _common/fragments/tcp-bbr.nix
+++ _common/fragments/tcp-bbr.nix
@@ -5,6 +5,7 @@
     "net.core.rmem_max" = 12582912;
     "net.ipv4.tcp_rmem" = "4096	524000	16777216";
     "net.ipv4.tcp_wmem" = "4096	524000	16777216";
+    "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
   };
 }
