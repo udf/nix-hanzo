3152f4f50bef1bd6e0843e7122e67346ad8c0dcc
udf <tabhooked@gmail.com>
2024-01-08 23:58:06 +0200

    update pihole

--- ananke/fragments/pihole.nix
+++ ananke/fragments/pihole.nix
@@ -4,7 +4,7 @@ let
 in
 {
   virtualisation.oci-containers.containers.pihole = {
-    image = "pihole/pihole:2023.11.0";
+    image = "pihole/pihole:2024.01.0";
     ports = [
       "53:53/tcp"
       "53:53/udp"
