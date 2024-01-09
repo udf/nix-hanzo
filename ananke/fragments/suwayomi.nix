c1aaa8da6738c69cf7f08ffd1892d0eabdfea50f
udf <tabhooked@gmail.com>
2024-01-09 17:51:21 +0200

    limit suwayomi memory even more
    when crash eta?

--- ananke/fragments/suwayomi.nix
+++ ananke/fragments/suwayomi.nix
@@ -13,11 +13,10 @@
       BASIC_AUTH_ENABLED = "true";
       BASIC_AUTH_USERNAME = "sam";
       BASIC_AUTH_PASSWORD = "hentai";
-      JDK_JAVA_OPTIONS = "-Xmx512m";
     };
     extraOptions = [
       "--hostname=${config.networking.hostName}"
-      "--memory=768m"
+      "--memory=512m"
     ];
   };
 
