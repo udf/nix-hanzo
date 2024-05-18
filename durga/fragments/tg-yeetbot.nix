232e74a312c8a4b6565e723b078d9b830fdaad81
udf <tabhooked@gmail.com>
2024-05-18 18:38:54 +0200

    this bot took me less than 4 hours to write, yet some poser couldnt even make it functional in 2 years
    suck it

---
+++ durga/fragments/tg-yeetbot.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+let
+  python-pkg = pkgs.python311.withPackages (ps: with ps; [
+    telethon
+  ]);
+in
+{
+  systemd.services.yeetbot = {
+    description = "Yeet Telegram Bot";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    path = [ python-pkg ];
+
+    serviceConfig = {
+      User = "yeetbot";
+      Type = "simple";
+      Restart = "always";
+      RestartSec = 5;
+      WorkingDirectory = "/home/yeetbot/adab2";
+      ExecStart = "${python-pkg}/bin/python bot.py";
+    };
+  };
+
+  users.extraUsers.yeetbot = {
+    description = "I beat my yacht and i yeet my bot";
+    home = "/home/yeetbot";
+    isNormalUser = true;
+    group = "yeetbot";
+    openssh.authorizedKeys.keys = config.users.users.sam.openssh.authorizedKeys.keys;
+  };
+  users.groups.yeetbot = { };
+}
