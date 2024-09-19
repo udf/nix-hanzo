74c44b89fa8d6ad88efa79f02b9e6be149aebdcc
udf <tabhooked@gmail.com>
2024-09-19 20:57:31 +0200

    upgrade suwayomi
    now with $100% more broken databases!

--- durga/fragments/suwayomi.nix
+++ durga/fragments/suwayomi.nix
@@ -6,7 +6,7 @@ let
 in
 {
   virtualisation.oci-containers.containers.suwayomi = {
-    image = "ghcr.io/suwayomi/tachidesk:v1.1.1-r1598";
+    image = "ghcr.io/suwayomi/tachidesk:v1.1.1-r1599";
     ports = [
       "${toString PORT}:${toString PORT}"
     ];
