413f88a3ab126f8cf6c40e27a69433f1f2db1ae0
udf <tabhooked@gmail.com>
2024-10-12 23:36:28 +0200

    use frigate's anonymous mode ui
    you wouldn't anon a moose

--- ananke/fragments/frigate.nix
+++ ananke/fragments/frigate.nix
@@ -3,8 +3,8 @@
   virtualisation.oci-containers.containers.frigate = {
     image = "ghcr.io/blakeblackshear/frigate:0.14.1";
     ports = [
-      "8971:8971" # Web UI
-      # "5000:5000" # Web UI (no auth)
+      # "8971:8971" # Web UI
+      "8971:5000" # Web UI (no auth)
       "8554:8554" # RTSP feeds
       "8555:8555/tcp" # WebRTC over tcp
       "8555:8555/udp" # WebRTC over udp
