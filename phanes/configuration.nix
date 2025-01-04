95541f02b60b2a716ede7bc0e9ebcbe0f099fe4a
udf <tabhooked@gmail.com>
2025-01-05 00:58:45 +0200

    use networkmanananager

--- phanes/configuration.nix
+++ phanes/configuration.nix
@@ -33,24 +33,14 @@
 
   services.openssh.enable = true;
 
+  users.users.sam.extraGroups = [ "networkmanager" ];
   networking = {
     hostId = "A5580085";
     hostName = "phanes";
-    defaultGateway = "192.168.0.1";
-    nameservers = [ "1.1.1.1" ];
-    interfaces.net0.ipv4.addresses = [{
-      address = "192.168.0.5";
-      prefixLength = 24;
-    }];
-    dhcpcd.enable = false;
-  };
-
-  systemd.network.links."10-net0" = {
-    matchConfig.PermanentMACAddress = "3c:18:a0:04:26:7e";
-    linkConfig = {
-      Name = "net0";
-      WakeOnLan = "magic";
+    networkmanager = {
+      enable = true;
     };
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 
   time.timeZone = "Africa/Harare";
