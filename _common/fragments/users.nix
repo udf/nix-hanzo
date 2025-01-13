b45d8ff37bcd93dec62596f4be1b04ce19321a86
udf <tabhooked@gmail.com>
2025-01-13 18:07:41 +0200

    add ios key

--- _common/fragments/users.nix
+++ _common/fragments/users.nix
@@ -8,6 +8,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzlWx6yy2nWV8fYcIm9Laap8/KxAlLJd943TIrcldSY archdesktop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/M7Ba3GQSuRFjMTInCAv/mZIvlc4KxyrJZkklL0yhv phone"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcNGC8Qg4+lVODt4cdbDtjbrVe44GGBae5sVoCZ1irJ sam@alice"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxBxAt/K3h+W4wyxLbTsW0awTIzJy2rpsQgDKxBHNe5 iOS"
       ];
       uid = 1000;
       packages = [ pkgs.python3 pkgs.python3Packages.pip ];
