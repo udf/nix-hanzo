4b7fe59fe029ac59c6cd0a616c35aa793271b012
udf <tabhooked@gmail.com>
2025-03-05 01:36:26 +0200

    disable LPM for ethernet adapter
    dont use usb ethernet, kids

--- phanes/configuration.nix
+++ phanes/configuration.nix
@@ -22,7 +22,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 1;
 
-  boot.kernelParams = [ "preempt=full" "usbcore.autosuspend=-1" ];
+  boot.kernelParams = [ "preempt=full" "usbcore.autosuspend=-1" "usbcore.quirks=17ef:7205:k" ];
   boot.kernel.sysctl = lib.mkForce {
     "vm.swappiness" = 15;
     "vm.overcommit_memory" = 1;
