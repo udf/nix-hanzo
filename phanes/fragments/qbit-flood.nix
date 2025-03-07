46052742259f3c5c7ae5227f1a5582fef4e9f1eb
udf <tabhooked@gmail.com>
2025-03-07 22:09:08 +0200

    add torrent completion script

--- phanes/fragments/qbit-flood.nix
+++ phanes/fragments/qbit-flood.nix
@@ -16,6 +16,13 @@
 
   services.watcher-bot.plugins = [ "flood" ];
 
+  environment.etc."qbit/on_done.sh".source = pkgs.writeScript "on_done.sh" ''
+    #!${pkgs.bash}/bin/bash
+    # on_done.sh "%F" "/path/to/dest/" {syncthing api key} {syncthing folder id}
+    cp -al "$1" "$2"
+    ${lib.getExe pkgs.curl} -X POST -H "X-API-Key:$3" "http://localhost:8384/rest/db/scan?folder=$4"
+  '';
+
   networking.firewall = {
     allowedTCPPorts = [ 32431 ];
     allowedUDPPorts = [ 32431 ];
