f3f1ab5e176b44d7f877a2dea32adf96a9008fe3
udf <tabhooked@gmail.com>
2025-03-10 01:45:56 +0200

    add plugin to log if wishlist results were found
    that was easy

---
+++ _common/helpers/nicotine-plugins/log-search-notifications/__init__.py
@@ -0,0 +1,14 @@
+from pynicotine.pluginsystem import BasePlugin
+from pynicotine.events import events
+
+
+class Plugin(BasePlugin):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    events.connect('show-search-notification', self.on_show_search_notification)
+
+  def disable(self):
+    events.disconnect('show-search-notification', self.on_show_search_notification)
+
+  def on_show_search_notification(self, search_token, message, title=None):
+    print(f'<4>Wishlist result found:\n{search_token}')
