185ad0388cd6af3de7937635d756c591f59b64d5
udf <tabhooked@gmail.com>
2025-03-15 17:38:03 +0200

    actually use access_log format
    lmao how long has it been like this

--- _common/modules/nginx.nix
+++ _common/modules/nginx.nix
@@ -96,10 +96,10 @@ in
             default "Something went wrong";
           }
 
-          access_log syslog:server=unix:/dev/log;
-          log_format   main '$remote_addr - $remote_user [$time_local] $status '
-            '"$request" $body_bytes_sent "$http_referer" '
-            '"$http_user_agent" "$http_x_forwarded_for"';
+          log_format main '$remote_addr ($http_x_forwarded_for) - $remote_user [$time_local] '
+            '"$request" $status $body_bytes_sent '
+            '"$http_referer" "$http_user_agent"';
+          access_log syslog:server=unix:/dev/log main;
         '';
 
       virtualHosts = mkMerge ([
