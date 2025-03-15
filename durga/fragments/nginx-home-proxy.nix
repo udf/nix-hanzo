ac01ed131e84cf017232e7b0f06b6dca4520b533
udf <tabhooked@gmail.com>
2025-03-15 17:38:29 +0200

    add recommended proxy settings

--- durga/fragments/nginx-home-proxy.nix
+++ durga/fragments/nginx-home-proxy.nix
@@ -32,6 +32,16 @@ in
             return 400;
           }
           resolver 1.1.1.1;
+
+          # don't let clients close the keep-alive connection to upstream. See the nginx blog for details:
+          # https://www.nginx.com/blog/avoiding-top-10-nginx-configuration-mistakes/#no-keepalives
+          proxy_http_version 1.1;
+          proxy_set_header "Connection" "";
+
+          proxy_connect_timeout 20s;
+          proxy_send_timeout 20s;
+          proxy_read_timeout 20s;
+
           proxy_pass https://${subMap};
           proxy_intercept_errors off;
           proxy_set_header X-Forwarded-Host $host;
