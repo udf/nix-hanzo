2c20f6c8cac55f4ef94f6a856e1a9f788ff634bf
udf <tabhooked@gmail.com>
2025-03-25 01:08:38 +0200

    actually prevent pinned pkgs repo from gc
    i hate nixos

--- phanes/fragments/unifi.nix
+++ phanes/fragments/unifi.nix
@@ -1,16 +1,20 @@
 { config, pkgs, lib, ... }:
 let
   # use pinned pkgs for mongodb to avoid compiling it each time
-  pinnedPkgs = import
-    (pkgs.fetchgit {
-      name = "nixos-24.11-2024-12-15";
-      url = "https://github.com/nixos/nixpkgs";
-      rev = "314e12ba369ccdb9b352a4db26ff419f7c49fa84";
-      hash = "sha256-5fNndbndxSx5d+C/D0p/VF32xDiJCJzyOqorOYW4JEo=";
-    })
-    { config.allowUnfree = true; };
+  pinnedRepoName = "nixos-24.11-2024-12-15";
+  pinnedPkgsRepo = pkgs.fetchgit {
+    name = pinnedRepoName;
+    leaveDotGit = true;
+    url = "https://github.com/nixos/nixpkgs";
+    rev = "314e12ba369ccdb9b352a4db26ff419f7c49fa84";
+    hash = "sha256-5fNndbndxSx5d+C/D0p/VF32xDiJCJzyOqorOYW4JEo=";
+  };
+  pinnedPkgs = import pinnedPkgsRepo { config.allowUnfree = true; };
 in
 {
+  # prevent repo from being garbage collected
+  environment.etc."${pinnedRepoName}".source = pinnedPkgsRepo;
+
   # enable to use desktop to build mongodb
   # (add "--max-jobs 0" to avoid building locally)
   # (also remember to move /tmp to disk to not run out of memory)
