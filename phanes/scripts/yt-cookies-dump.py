22a0893be8eaa14e375b0bcc9c79173a5fed523a
udf <tabhooked@gmail.com>
2025-05-17 02:34:36 +0200

    remove unused script

--- phanes/scripts/yt-cookies-dump.py
+++
@@ -1,6 +0,0 @@
-from http.cookiejar import MozillaCookieJar
-
-import browser_cookie3
-
-cookiejar = browser_cookie3.firefox(domain_name='youtube.com')
-MozillaCookieJar.save(cookiejar, filename='/dev/stdout')
