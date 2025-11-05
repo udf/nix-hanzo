40fdeeef06b5bf6282264c21ce6620578462f005
udf <tabhooked@gmail.com>
2025-11-05 22:06:41 +0200

    add mashiro key

--- _common/fragments/users.nix
+++ _common/fragments/users.nix
@@ -9,9 +9,13 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/M7Ba3GQSuRFjMTInCAv/mZIvlc4KxyrJZkklL0yhv phone"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcNGC8Qg4+lVODt4cdbDtjbrVe44GGBae5sVoCZ1irJ sam@alice"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxBxAt/K3h+W4wyxLbTsW0awTIzJy2rpsQgDKxBHNe5 iOS"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEbHvYANduDUO7939VJu12KFxMEM5fCRx/4PG/W5UwIa sam@mashiro"
       ];
       uid = 1000;
-      packages = [ pkgs.python3 pkgs.python3Packages.pip ];
+      packages = [
+        pkgs.python3
+        pkgs.python3Packages.pip
+      ];
     };
   };
 }
