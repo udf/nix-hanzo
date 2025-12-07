9f38c53382e64f6ffa5ce902312d9bd932fe0d2c
udf <tabhooked@gmail.com>
2025-12-07 13:35:24 +0200

    update suwayomi

--- durga/fragments/suwayomi.nix
+++ durga/fragments/suwayomi.nix
@@ -6,7 +6,7 @@ let
 in
 {
   virtualisation.oci-containers.containers.suwayomi = {
-    image = "ghcr.io/suwayomi/tachidesk:v2.1.1933";
+    image = "ghcr.io/suwayomi/tachidesk:v2.1.2031";
     ports = [
       "127.0.0.1:${toString PORT}:${toString PORT}"
     ];
