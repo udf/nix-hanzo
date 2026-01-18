5272842a1f65f770d249a2b29a5004088b15565f
udf <tabhooked@gmail.com>
2026-01-18 16:13:13 +0200

    use nix-shell for asn db generator

--- _common/constants/gen_asn_db.sh
+++ _common/constants/gen_asn_db.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p python3Packages.pyasn
 set -e
 pyasn_util_download.py --latest
 pyasn_util_convert.py --single rib.*.bz2 ipasn.dat
 pyasn_util_asnames.py -o asnames.json
 rm rib.*.bz2
-gzip --best ipasn.dat
+gzip -f --best ipasn.dat
